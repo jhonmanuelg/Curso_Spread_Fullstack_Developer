143c170803c7d0645401047b68f3388cda1006a2
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-06 03:28:18 -0300

    Variáveis e Tipos Constantes

---
+++ Introdução ao JavaScript/Constantes.js
@@ -0,0 +1,15 @@
+// Variáveis e Tipos Constantes
+
+// const FIRST_NOME = "jhon";
+// const FIRST_NOME = "karla"; === NÃO SE PODE VOLCER A DECLARA MIM RETRIBUIR ELA
+// const FIRST_NOME = "pedro"; === NÃO SE PODE VOLCER A DECLARA MIM RETRIBUIR ELA
+
+
+const FIRST_NOME = "jhon"; //da forma que fue inicilaiçada ela vai ficar assim siempre
+
+//FIRST_NOME = "jhon"; // declarada metodo Hoisting retribuindo não se pode
+
+console.log(FIRST_NOME)
+
+
+// const FIRST_NOME;  indica que pressisa sua inicialisacion
