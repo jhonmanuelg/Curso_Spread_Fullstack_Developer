be74a064b262c10cc1cc61b1ef5c0a6db11f78c8
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-06-27 13:40:21 -0300

    Subindo: modificação de pasta

--- Introdução ao JavaScript/estrutura-Métodos-Set-vs-Array.js
+++ Introdução ao JavaScript/estrutura-Métodos-Set-vs-Array.js
@@ -1,17 +1,3 @@
 // Coleções cEstrutura, Métodos e Set vs Array
 
 
-colecoes.beforeEnter(entra) {
-    console.log('beforeEnter');
-},
-enter(el, done) {
-    console.log('enter');
-    done();
-},
-beforeLeave(el) {
-    console.log('beforeLeave');
-},
-leave(el, done) {
-    console.log('leave');
-    done();
-},
